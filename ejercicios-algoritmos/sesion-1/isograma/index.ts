fd551acc08f6186b716101a342ba6715d0fa3ea1
Emanuel Gauler <emanuelgauler@gmail.com>
2025-03-18 22:32:36 -0300

    implementación básica terminada

--- ejercicios-algoritmos/sesion-1/isograma/index.ts
+++ ejercicios-algoritmos/sesion-1/isograma/index.ts
@@ -1,4 +1,10 @@
 export default function esIsograma(string: string): boolean {
-  // TODO: implement
-  return false;
+   const count = string.split("")
+   .map( e => e.toLowerCase() )
+   .reduce((a, e) => {
+      a[e] = a[e] + 1 || 1;
+      return a;
+   }, {});
+
+   return Object.values(count).every( e => e === 1 )
 }
